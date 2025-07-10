b6c4b2843b1401bfb5889bb56eaec3668703e0b7
mani-hash <manimehalan400@gmail.com>
2025-07-10 14:38:05 +0530

    feat: base service providers [TASK-3]

---
+++ app/Providers/DatabaseServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use Library\Framework\Core\Application;
+use Library\Framework\Database\Connection;
+
+/**
+ * Database Service Provider
+ * 
+ * Registers core database connections and
+ * loads relavant entries
+ * 
+ * NOTE for cs 28 members: If you need to add
+ * custom code after registering this service provider, then
+ * add them inside boot method.
+ */
+class DatabaseServiceProvider extends Provider
+{
+    /**
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * @param \Library\Framework\Core\Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register database connections
+     * @return void
+     */
+    public function register()
+    {
+        $config = config('database.connections.pgsql');
+
+        $this->app->singleton(Connection::class, function () use ($config) {
+            return new Connection($config);
+        });
+    }
+
+    public function boot()
+    {
+
+    }
+}
---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Providers;
+
+use App\Middleware\StartSessionMiddleware;
+use Library\Framework\Core\Application;
+use Library\Framework\Routing\Router;
+
+/**
+ * Route Service Provider
+ * 
+ * Registers the router which handles
+ * routing for the web application and loads
+ * all the routes for the web app.
+ * 
+ * NOTE: To register middlewares, include them
+ * in the setup method. Only middlewares registered here will
+ * be available!
+ */
+class RouteServiceProvider extends Provider
+{
+    /**
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * @param \Library\Framework\Core\Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register all middlewares and global middlewares.
+     * @param \Library\Framework\Routing\Router $router Base router class
+     * @param array $middlewares All middlewares that needs to be registered
+     * @param array $globalMiddlewares
+     * @return void
+     */
+    private function setup(Router $router, array $middlewares = [], array $globalMiddlewares = [])
+    {
+        foreach ($middlewares as $name => $class) {
+            $router->registerMiddleware($name, $class);
+        }
+
+        foreach ($globalMiddlewares as $name) {
+            $router->globalMiddleware($name);
+        }
+    }
+    
+    /**
+     * Register base router class
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Router::class, function () {
+            return new Router;
+        });
+
+        // $this->app->bind('router', Router::class);
+    }
+
+    /**
+     * Register middleware and load web routes
+     * @return void
+     */
+    public function boot()
+    {
+        $router = $this->app->make(Router::class);
+
+        // register middlewares
+        $this->setup($router, [
+            'session' => StartSessionMiddleware::class,
+        ], [
+            'session',
+        ]);
+
+        $routes = require __DIR__ . '/../../routes/web.php'; // import all web routes
+
+        // Boot all web routes
+        foreach ($routes as $route) {
+            list($method, $uri, $action, $middleware) = array_pad($route, 4, []);            
+
+            $router->addRoute($method, $uri, $action, $middleware);
+        }
+    }
+}
