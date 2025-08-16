530a9a32c35a02fe3019bac4a6c522c421b2effb
mani-hash <manimehalan400@gmail.com>
2025-08-16 23:09:15 +0530

    feat: base auth layout [TASK-71]

---
+++ resources/views/layout/auth.view.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/app.css">
+    @yield('css')
+</head>
+<body>
+    @yield('content')
+    @yield('scripts')
+</body>
+</html>
