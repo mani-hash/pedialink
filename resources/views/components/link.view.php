b8f7ad5100831fae3c509a25c06c9aae59a0ef04
mani-hash <manimehalan400@gmail.com>
2025-08-20 10:12:28 +0530

    feat: link css and component file [TASK-71]

---
+++ resources/views/components/link.view.php
@@ -0,0 +1,45 @@
+<?php
+
+    $id = !empty($id) ? "id={$id}" : "";
+
+    $classes = 'link';
+
+    if (!empty($class)) {
+        $classes = ' ' . $class;
+    }
+
+    if (!empty($type)) {
+        $classes .= " link-{$type}";
+    } else {
+        $classes .= " link-plain";
+    }
+
+    if (!empty($size)) {
+        $classes .= " link-{$size}";
+    }
+
+     if (!empty($slots['icon']) && trim($slot) === '') { 
+        $classes .= ' link-icon-only'; 
+    }
+
+    $targetAttr = !empty($target) ? "target='{$target}'" : "";
+    $relAttr = !empty($rel) ? "rel='{$rel}'" : "";
+    $ariaDisabledAttr = !empty($ariadisabled) ? "aria-disabled='true' tabindex='-1'" : "";
+?>
+
+<a
+    {{ $id }}
+    href="{{ $href }}"
+    class="{{ $classes }}"
+    {{ $targetAttr }} {{ $relAttr }} {{ $ariaDisabledAttr }}
+>
+    @if(!empty($slots['icon']))
+        <span class="link-icon">
+        {{ $slots["icon"] }}
+        </span>
+    @endif
+
+    @if(trim($slot) !== '')
+        <span>{{ $slot }}</span>
+    @endif
+</a>
