d8482d84c2afd8ad06c1c63d82ff57f55f3245d1
mani-hash <manimehalan400@gmail.com>
2025-08-20 19:47:52 +0530

    feat: parent login page [TASK-71]

--- resources/views/pages/auth/parent-login.view.php
+++ resources/views/pages/auth/parent-login.view.php
@@ -1,6 +1,60 @@
 @extends('layout/auth')
 
+@section('title')
+    Parent Login
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/pages/auth/parent-login.css') }}">
+@endsection
+
 @section('content')
-parent login
+    <div id="main">
+        <div>
+            <c-link class="return-link-group">
+                <c-slot name="icon">
+                    <svg width="25px" height="25px" viewBox="0 0 1024 1024" fill="#000000" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill="" /></svg>
+                </c-slot>
+                Go Back
+            </c-link>
+        </div>
+        <div class="form-container">
+            <div class="topic">
+                <h1>Welcome Back</h1>
+                <span class="text-muted">Sign in to your account to continue</span>
+            </div>
+            <c-card class="form-content">
+                <div class="tab-links">
+                    <c-link type="subtle" class="active">
+                        Parent
+                    </c-link>
+                    <c-link type="subtle">
+                        Staff
+                    </c-link>
+                </div>
+                <div>
+                    <form id="parent-login" action="">
+                        <c-input label="Email:" type="text" />
+                        <c-input label="Password:" type="password" />
+                    </form>
+                    <div class="forgot-password-group">
+                        <c-link size="sm">
+                            Forgot Password?
+                        </c-link>
+                    </div>
+                </div>
+                <div class="footer">
+                    <c-button id="sign-in" form="parent-login" type="primary">
+                        Sign In
+                    </c-button>
+                    <div class="sign-up-group">
+                        <span class="text-muted">Don't have an account?</span>
+                        <c-link>Sign up</c-link>
+                    </div>
+                </div>
+            </c-card>
+        </div>
+
+    </div>
 @endsection
 
