cb74ef2888a5558068d799ec84f75b0b9dd1ef38
mani-hash <manimehalan400@gmail.com>
2025-08-23 16:40:26 +0530

    feat: added method for initial parent form verify route and further security updates [TASK-71]

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,14 +3,70 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use App\Services\AuthService;
 use Library\Framework\Http\Request;
 use Library\Framework\Http\Response;
+use Library\Framework\Session\SessionManager;
 
 class AuthController
 {
-    public function parentRegister(Request $request)
+    private AuthService $authService;
+
+    public function __construct()
     {
+        $this->authService = new AuthService();
+    }
+
+    public function parentRegisterInitial(Request $request)
+    {
+        if ($registerFormSession = session()->get("register_form", null)) {
+            $registerFormSession["passwordHash"] = null;
+            session()->set("register_form", $registerFormSession);
+
+            return view('auth/parent-register', [
+                "data" => $registerFormSession,
+            ]);
+        }
         return view('auth/parent-register');
+    }
+
+    public function parentVerifyInitial(Request $request)
+    {
+        $data = [
+            "firstName" => $request->input("firstName"),
+            "lastName" => $request->input("lastName"),
+            "email" => $request->input("email"),
+        ];
+
+        $errors = $this->authService->validateInitialForm(array_merge($data, [
+            "password" => $request->input("password"),
+            "confirmPassword" => $request->input("confirmPassword"),
+        ]));
+
+        if (count($errors) !== 0) {
+            session()->set("register_form", array_merge($data, [
+                "passwordHash" => password_hash(
+                    $request->input("password"), PASSWORD_DEFAULT
+                ),
+            ]));
+            return redirect(route("parent.register.final"));
+        }
+
+        return redirect(route("parent.register", [
+            "data" => $data,
+            "errors" => $errors,
+        ]));
+    }
+
+    public function parentRegisterFinal(Request $request)
+    {
+        if (session()->get("register_form", null)) {
+            return view('auth/parent-register', [
+                "final" => true,
+            ]);
+        }
+
+        return redirect(route("parent.register"));
     }
 
     public function parentLogin(Request $request)
