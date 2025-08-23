e1e5438ba0ea57512b0c0b7c25b8e3509835f0eb
mani-hash <manimehalan400@gmail.com>
2025-08-23 16:38:08 +0530

    feat: updated parent register route and added route for next form [TASK-71]

--- routes/sectors/auth.php
+++ routes/sectors/auth.php
@@ -3,7 +3,8 @@
 use App\Controllers\AuthController;
 
 return [
-    ['GET', '/parent/register', [AuthController::class, 'parentRegister'], 'parent.register', ['guest']],
+    ['GET', '/parent/register', [AuthController::class, 'parentRegisterInitial'], 'parent.register', ['guest']],
+    ['GET', '/parent/register/final', [AuthController::class, 'parentRegisterFinal'], 'parent.register.final', ['guest']],
     ['GET', '/parent/login', [AuthController::class, 'parentLogin'], 'parent.login', ['guest']],
     ['GET', '/staff/login', [AuthController::class, 'staffLogin'], 'staff.login', ['guest']],
     ['GET', '/forgot-password', [AuthController::class, 'forgotPassword'], 'forgot.password', ['guest']],
