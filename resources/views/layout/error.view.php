632ed68f300b456642652c028549ac3c2dc24c40
mani-hash <manimehalan400@gmail.com>
2025-08-24 02:12:12 +0530

    feat: base error layout [TASK-71]

---
+++ resources/views/layout/error.view.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('css')
+</head>
+<body>
+    @yield('content')
+    @yield('scripts')
+</body>
+</html>
