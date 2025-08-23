0e6bb793d4acf4e0e017639473857894c37ca30b
mani-hash <manimehalan400@gmail.com>
2025-08-23 22:55:28 +0530

    feat: post routes for registration [TASK-71]

--- routes/sectors/auth.php
+++ routes/sectors/auth.php
@@ -4,7 +4,9 @@ use App\Controllers\AuthController;
 
 return [
     ['GET', '/parent/register', [AuthController::class, 'parentRegisterInitial'], 'parent.register', ['guest']],
+    ['POST', '/parent/register', [AuthController::class, 'parentVerifyInitial'], 'parent.register.submit', ['guest']],
     ['GET', '/parent/register/final', [AuthController::class, 'parentRegisterFinal'], 'parent.register.final', ['guest']],
+    ['POST', '/parent/register/final', [AuthController::class, 'parentVerifyFinal'], 'parent.register.final.submit', ['guest']],
     ['GET', '/parent/login', [AuthController::class, 'parentLogin'], 'parent.login', ['guest']],
     ['GET', '/staff/login', [AuthController::class, 'staffLogin'], 'staff.login', ['guest']],
     ['GET', '/forgot-password', [AuthController::class, 'forgotPassword'], 'forgot.password', ['guest']],
