1256c2a71be9327821f7e5cc818e8446d385886a
mani-hash <manimehalan400@gmail.com>
2025-08-24 13:28:53 +0530

    feat: role based access control middlewares [TASK-71]

---
+++ app/Middleware/ParentMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Middleware;
+
+use Library\Framework\Http\Request;
+
+/**
+ * Middleware for authenticated pages
+ * that is authorized only for parent accounts
+ */
+class ParentMiddleware
+{
+    public function handle(Request $request, callable $next, array $params)
+    {
+        if (auth()->check() && auth()->user()->isParent()) {
+            return $next($request, $params);
+        }
+
+        return view('error/404'); // temporary fallback for errors (Need to implement proper Error class!)
+    }
+}
