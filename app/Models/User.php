c49a8be2911d11e0b16be21386da0444d437be88
mani-hash <manimehalan400@gmail.com>
2025-08-24 13:28:12 +0530

    feat: methods to verify current user roles [TASK-71]

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,5 +7,41 @@ use Library\Framework\Core\Model;
 class User extends Model
 {
     protected static string $table = "users";
-    protected array $fillable = ["name", "email"];
+    protected array $fillable = ["name", "role", "email"];
+
+    public function isParent()
+    {
+        if ($this->role === "parent") {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isPublicHealthMidwife()
+    {
+        if ($this->role === "phm") {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isDoctor()
+    {
+        if ($this->role === "doctor") {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isAdmin()
+    {
+        if ($this->role === "admin") {
+            return true;
+        }
+
+        return false;
+    }
 }
