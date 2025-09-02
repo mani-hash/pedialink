eb7c504c16711154c352d506c290443576b4fa53
Keeththi2003 <keeththi2003@gmail.com>
2025-09-02 16:00:48 +0530

    feat: dashboard view [TASK-82]

--- resources/views/pages/parent/dashboard.view.php
+++ resources/views/pages/parent/dashboard.view.php
@@ -4,18 +4,32 @@
 Parent Dashboard
 @endsection
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/pages/parent/dashboard.css') }}">
+@endsection
+
 
 @section('header')
 Good Evening, 
 <br>Harry
-<c-card>
-    Hi
-</c-card>
+<section class="pill-container">
+    <c-card>
+        <div class="text-section">
+        <span class="pill-title">Linked Children</span>
+        <span class="pill-subtitle">03</span>
+        <img src="{{asset('assets/icons/baby-01.svg')}}" alt="">
+</div>
+    </c-card>
+
+
+</section>
+
 @endsection
 
 
 @section('content')
-<div class="container">
+
+<main class="container">
     <c-card class="card">
         <div class="header">
             <div class="title-section">
@@ -25,13 +39,57 @@ Good Evening,
             <c-button varient="secondary">View All</c-button>
         </div>
         <div class="card-body">
-            <div class="tab">
+            <div class="tab vaccine">
                 
             </div>
         </div>
-        
     </c-card>
-</div>
+       <c-card class="card">
+        <div class="header">
+            <div class="title-section">
+                <span class="card-title">Child Growth Chart</span>
+                <span class="card-subtitle">Track Baby Sarah's BMI over time</span>
+            </div>
+ <c-select name='child' class="child-select" placeholder="Select Child">
+ *    <li class="select-item" data-value="baby-sara">Baby Sara</li>
+ *    <li class="select-item" data-value="baby-john">Baby John</li>
+ * </c-select>        </div>
+        <div class="card-body">
+            <div class="tab events-campaigns">
+                
+            </div>
+        </div>
+    </c-card>
+    <c-card class="card">
+        <div class="header">
+            <div class="title-section">
+                <span class="card-title">Upcoming Appoinments</span>
+                <span class="card-subtitle">Your scheduled visits to the clinic</span>
+            </div>
+            <c-button varient="secondary">View Schedule</c-button>
+        </div>
+        <div class="card-body">
+            <div class="tab appoinment">
+                
+            </div>
+        </div>
+    </c-card>
+    <c-card class="card">
+        <div class="header">
+            <div class="title-section">
+                <span class="card-title">Upcoming Events & Campaigns</span>
+                <span class="card-subtitle">Scheduled health events and vaccination drives</span>
+            </div>
+            <c-button varient="secondary">View All</c-button>
+        </div>
+        <div class="card-body">
+            <div class="tab events-campaigns">
+                
+            </div>
+        </div>
+    </c-card>
+    
+</main>
 
 
 @endsection
