9d10e38c07bd6aea74c191b69635a704445b4e6c
Keeththi2003 <keeththi2003@gmail.com>
2025-09-02 11:15:54 +0530

    feat:sidebar routes added [TASK-83]

--- routes/sectors/doctor.php
+++ routes/sectors/doctor.php
@@ -4,4 +4,9 @@ use App\Controllers\DoctorController;
 
 return [
     ['GET', '/doctor/dashboard', [DoctorController::class, 'dashboard'], 'doctor.dashboard', ['doctor']],
+    ['GET', '/doctor/child-profiles', [DoctorController::class, 'childProfiles'], 'doctor.child.profiles', ['doctor']],
+    ['GET', '/doctor/maternal-profiles', [DoctorController::class, 'maternalProfiles'], 'doctor.maternal.profiles', ['doctor']],
+    ['GET', '/doctor/appointments', [DoctorController::class, 'appointments'], 'doctor.appointments', ['doctor']],
+    ['GET', '/doctor/notifications', [DoctorController::class, 'notifications'], 'doctor.notifications', ['doctor']],
+    ['GET', '/doctor/settings', [DoctorController::class, 'settings'], 'doctor.settings', ['doctor']],
 ];
