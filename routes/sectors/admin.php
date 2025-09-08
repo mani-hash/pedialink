633363a8afd372d7a3bb791aca43e55174ded6ba
mani-hash <manimehalan400@gmail.com>
2025-09-08 17:52:11 +0530

    feat: added admin routes [TASK-90]

--- routes/sectors/admin.php
+++ routes/sectors/admin.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Controllers\Admin\ChildController;
+use App\Controllers\Admin\UserController;
 use App\Controllers\AdminController;
 
 return [
     ['GET', '/admin/dashboard', [AdminController::class, 'dashboard'], 'admin.dashboard', ['admin']],
+    ['GET', '/admin/user/overview', [UserController::class, 'overview'], 'admin.user.overview', ['admin']],
+    ['GET', '/admin/user/parent', [UserController::class, 'parentAccountApproval'], 'admin.user.parent', ['admin']],
+    ['GET', '/admin/user/admin', [UserController::class, 'admin'], 'admin.user.admin', ['admin']],
+    ['GET', '/admin/child-profiles/overview', [ChildController::class, 'overview'], 'admin.child.overview', ['admin']],
 ];
