0af359dafd15f256227eb957624e02055c3dc29a
mani-hash <manimehalan400@gmail.com>
2025-09-09 16:36:19 +0530

    update: changed controller name [TASK-90]

--- routes/sectors/admin.php
+++ routes/sectors/admin.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Controllers\Admin\ChildController;
+use App\Controllers\Admin\DashboardController;
 use App\Controllers\Admin\UserController;
-use App\Controllers\AdminController;
 
 return [
-    ['GET', '/admin/dashboard', [AdminController::class, 'dashboard'], 'admin.dashboard', ['admin']],
+    ['GET', '/admin/dashboard', [DashboardController::class, 'index'], 'admin.dashboard', ['admin']],
     ['GET', '/admin/user/overview', [UserController::class, 'overview'], 'admin.user.overview', ['admin']],
     ['GET', '/admin/user/parent', [UserController::class, 'parentAccountApproval'], 'admin.user.parent', ['admin']],
     ['GET', '/admin/user/admin', [UserController::class, 'admin'], 'admin.user.admin', ['admin']],
