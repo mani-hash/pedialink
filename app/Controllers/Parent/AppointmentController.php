1f17f7cb83a336685b7177c546b26ef97c55ca9c
Keeththi2003 <keeththi2003@gmail.com>
2025-09-13 20:07:49 +0530

    feat: enhance appointment details modal with structured layout and additional information [TASK-82]

--- app/Controllers/Parent/AppointmentController.php
+++ app/Controllers/Parent/AppointmentController.php
@@ -8,4 +8,6 @@ class AppointmentController
     {
         return view("parent/appointments");
     }
+
+ 
 }
--- resources/views/pages/parent/appointments.view.php
+++ resources/views/pages/parent/appointments.view.php
@@ -94,17 +94,21 @@ $appointments = [
     <c-slot name="extrabtn">
         <c-link type="primary">
             <c-slot name="icon">
-<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="M2.08337 10C2.08337 6.26806 2.08337 4.40208 3.24274 3.24271C4.40211 2.08334 6.26809 2.08334 10 2.08334C13.732 2.08334 15.598 2.08334 16.7573 3.24271C17.9167 4.40208 17.9167 6.26806 17.9167 10C17.9167 13.732 17.9167 15.5979 16.7573 16.7573C15.598 17.9167 13.732 17.9167 10 17.9167C6.26809 17.9167 4.40211 17.9167 3.24274 16.7573C2.08337 15.5979 2.08337 13.732 2.08337 10Z"
-            stroke="#FAFAFA" stroke-width="1.5" />
-        <path d="M9.16663 5.83334L14.1666 5.83334" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
-        <path d="M5.83337 5.83334L6.66671 5.83334" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
-        <path d="M5.83337 10L6.66671 10" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
-        <path d="M5.83337 14.1667L6.66671 14.1667" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
-        <path d="M9.16663 10L14.1666 10" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
-        <path d="M9.16663 14.1667L14.1666 14.1667" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
-    </svg>            </c-slot>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M2.08337 10C2.08337 6.26806 2.08337 4.40208 3.24274 3.24271C4.40211 2.08334 6.26809 2.08334 10 2.08334C13.732 2.08334 15.598 2.08334 16.7573 3.24271C17.9167 4.40208 17.9167 6.26806 17.9167 10C17.9167 13.732 17.9167 15.5979 16.7573 16.7573C15.598 17.9167 13.732 17.9167 10 17.9167C6.26809 17.9167 4.40211 17.9167 3.24274 16.7573C2.08337 15.5979 2.08337 13.732 2.08337 10Z"
+                        stroke="#FAFAFA" stroke-width="1.5" />
+                    <path d="M9.16663 5.83334L14.1666 5.83334" stroke="#FAFAFA" stroke-width="1.5"
+                        stroke-linecap="round" />
+                    <path d="M5.83337 5.83334L6.66671 5.83334" stroke="#FAFAFA" stroke-width="1.5"
+                        stroke-linecap="round" />
+                    <path d="M5.83337 10L6.66671 10" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
+                    <path d="M5.83337 14.1667L6.66671 14.1667" stroke="#FAFAFA" stroke-width="1.5"
+                        stroke-linecap="round" />
+                    <path d="M9.16663 10L14.1666 10" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" />
+                    <path d="M9.16663 14.1667L14.1666 14.1667" stroke="#FAFAFA" stroke-width="1.5"
+                        stroke-linecap="round" />
+                </svg> </c-slot>
             Requset Appointment </c-link>
 
     </c-slot>
@@ -160,7 +164,58 @@ $appointments = [
                                 </c-button>
                             </c-slot>
                             <c-slot name="menu">
-                                <c-dropdown.item>View Details</c-dropdown.item>
+                                <c-modal size="md" :initOpen="false">
+                                    <c-slot name="trigger">
+                                        <c-dropdown.item>View Details</c-dropdown.item>
+                                    </c-slot>
+
+                                    <c-slot name="headerPrefix">
+                                        <img src="{{ asset('assets/icons/profile.svg' )}}" />
+                                    </c-slot>
+
+                                    <c-slot name="header">
+                                        <div>Appointment Details</div>
+                                    </c-slot>
+
+                                    <c-slot name="headerSuffix">
+
+                                        <c-badge type="{{ $badgeType }}">
+                                            {{$appointmnet['status']}}
+                                        </c-badge>
+                                    </c-slot>
+
+
+
+                                    <c-modal.viewcard>
+                                        <c-modal.viewitem icon="{{ asset('assets/icons/profile.svg') }}"
+                                            title="Appointment ID" info="{{ $appointmnet['id'] }}" />
+                                        <c-modal.viewitem icon="{{ asset('assets/icons/baby-01.svg') }}"
+                                            title="Requester Name" info="{{ $appointmnet['name'] }}" />
+                                        <c-modal.viewitem icon="{{ asset('assets/icons/calendar-03.svg') }}"
+                                            title="Date" info="{{ $appointmnet['date'] }} " />
+                                        <c-modal.viewitem icon="{{ asset('assets/icons/clock-01.svg') }}" title="Time"
+                                            info="{{ $appointmnet['time'] }}" />
+                                        <c-modal.viewitem icon="{{ asset('assets/icons/location-05.svg') }}"
+                                            title="Location" info="{{ $appointmnet['location'] }}" />
+                                        <c-modal.viewitem icon="{{ asset('assets/icons/doctor.svg') }}" title="Doctor"
+                                            info="{{ $appointmnet['doctor'] }}" />
+
+
+
+                                    </c-modal.viewcard>
+
+
+
+                                    <c-slot name="footer">
+                                        <c-button varient="primary">
+                                            Cancel Appointment
+                                        </c-button>
+
+                                        <c-button varient="primary">
+                                            Reschedule Appointment
+                                        </c-button>
+                                    </c-slot>
+                                </c-modal>
                                 <c-dropdown.sep />
 
                                 <c-dropdown.item>
