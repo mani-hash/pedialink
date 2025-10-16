77fe616fd28594a8ce0c1db3c02c3608c3a87458
Keeththi2003 <keeththi2003@gmail.com>
2025-10-16 12:54:44 +0530

    feat: implement VaccinationController and update routing for vaccination endpoint [TASK-82]

---
+++ app/Controllers/Parent/VaccinationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Parent;
+
+class VaccinationController
+{
+    public function index()
+    {
+        return view("parent/vaccination");
+    }
+   
+ 
+}
--- routes/sectors/parent.php
+++ routes/sectors/parent.php
@@ -5,13 +5,14 @@ use App\Controllers\Parent\DashboardController;
 use App\Controllers\Parent\EventController;
 use App\Controllers\Parent\MyChildrenController;
 use App\Controllers\Parent\NutritionController;
+use App\Controllers\Parent\VaccinationController;
 use App\Controllers\ParentController;
 
 return [
     ['GET', '/parent/dashboard', [DashboardController::class, 'index'], 'parent.dashboard', ['parent']],
     ['GET', '/parent/my-children', [MyChildrenController::class, 'index'], 'parent.my.children', ['parent']],
     ['GET', '/parent/my-children/{id}', [MyChildrenController::class, 'viewChildDetails'], 'parent.child.details', ['parent']],
-    ['GET', '/parent/vaccination', [ParentController::class, 'vaccination'], 'parent.vaccination', ['parent']],
+    ['GET', '/parent/vaccination', [VaccinationController::class, 'index'], 'parent.vaccination', ['parent']],
     ['GET', '/parent/nutrition-tracking', [NutritionController::class, 'index'], 'parent.nutrition.tracking', ['parent']],
     ['GET', '/parent/appointments', [AppointmentController::class, 'index'], 'parent.appointments', ['parent']],
     ['POST', '/parent/request-appointment', [AppointmentController::class, 'requestAppointment'], 'parent.request.appointment', ['parent']],
