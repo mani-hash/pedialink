5ea77d612afd21aac4cd6f4aece94a177b831a0f
mani-hash <manimehalan400@gmail.com>
2025-10-18 17:16:22 +0530

    feat: added service class logic to admin method [TASK-105]

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -1,9 +1,17 @@
 <?php
 
 namespace App\Controllers\Admin;
+use App\Services\Admin\AdminUserService;
 
 class UserController
 {
+    private AdminUserService $adminUserService;
+
+    public function __construct()
+    {
+        $this->adminUserService = new AdminUserService();
+    }
+
     public function overview()
     {
         return view('admin/user/overview');
@@ -16,6 +24,10 @@ class UserController
 
     public function admin()
     {
-        return view('admin/user/admin');
+        $admins = $this->adminUserService->getAdminDetails();
+
+        return view('admin/user/admin', [
+            "admins" => $admins,
+        ]);
     }
 }
