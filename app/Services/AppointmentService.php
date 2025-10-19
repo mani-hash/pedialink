5d2b9692f39a7e0fc91be28b8079dadb9c1b6bab
Keeththi2003 <keeththi2003@gmail.com>
2025-10-19 11:59:26 +0530

    feat: add validation methods for appointment date and time in AppointmentService [TASK-107]

--- app/Services/AppointmentService.php
+++ app/Services/AppointmentService.php
@@ -8,8 +8,9 @@ use App\Models\Appointment;
 class AppointmentService
 {
 
-    public function getAppointmentDetails(){
-         $appointments = Appointment::query()->get();
+    public function getAppointmentDetails()
+    {
+        $appointments = Appointment::query()->get();
 
         $resource = [];
 
@@ -30,19 +31,74 @@ class AppointmentService
 
         return $resource;
     }
-
-    public function createAppointment(array $data): Appointment
+    public function validateDate($date)
     {
-        $appointment = new Appointment();
-        foreach ($data as $key => $value) {
-            if (in_array($key, $appointment->fillable)) {
-                $appointment->attributes[$key] = $value;
-            }
+        $error = null;
+
+        if (!Validator::validateFieldExistence($date)) {
+            $error = "Appointment Date cannot be empty";
+            return $error;
         }
-        $appointment->save();
-        return $appointment;
+
+
+        $appointmentDate = new \DateTime($date);
+
+         if (!$appointmentDate) {
+        $error = "Invalid time format (expected HH:MM, 24-hour format)";
+        return $error;
+    }
+        $currentDate = new \DateTime();
+
+        if ($appointmentDate <= $currentDate) {
+
+            $error = "Appointment date must be in the future";
+            return $error;
+        }
+
+
+        $dayOfWeek = (int) $appointmentDate->format('N');
+
+        if ($dayOfWeek < 1 || $dayOfWeek > 5) {
+            $error = "Appointments are only available on weekdays (Monday to Friday)";
+            return $error;
+        }
+
+
+        return $error;
+
+
+    }
+
+    public function validateTime($time)
+    {
+        $error = null;
+
+        if (!Validator::validateFieldExistence($time)) {
+            $error = "Appointment Time cannot be empty";
+            return $error;
+        }
+
+
+
+        $appointmentTime = \DateTime::createFromFormat('H:i', $time);
+
+        if (!$appointmentTime) {
+            return "Invalid time format (expected HH:MM, 24-hour format)";
+        }
+        $startTime = \DateTime::createFromFormat('H:i', '09:00');
+        $endTime = \DateTime::createFromFormat('H:i', '17:00');
+
+        if ($appointmentTime < $startTime || $appointmentTime > $endTime) {
+            $error = "Appointment Time must be within working hours (09:00 - 17:00)";
+            return $error;
+        }
+
+
+
+        return $error;
+
+
     }
 }
-
 
 ?>
