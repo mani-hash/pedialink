256f41e043203f75f32a9682b2921ae20ca686bc
Keeththi2003 <keeththi2003@gmail.com>
2025-10-22 00:24:47 +0530

    feat: add method to retrieve maternal statistics by maternal ID in MaternalStatService [TASk-119]

--- app/Services/MaternalStatService.php
+++ app/Services/MaternalStatService.php
@@ -30,6 +30,31 @@ class MaternalStatService
         return $resource;
     }
 
-}
 
-?>
+
+    public function getMaternalStatByMaternalId($id)
+    {
+        $maternalStats = MaternalStat::query()->where('maternal_id', $id)->get();
+
+        $resource = [];
+        foreach ($maternalStats as $stat) {
+            $resource[] = [
+                'id' => $stat->id,
+                'maternal_id' => $stat->maternal_id,
+                'visit_date' => $stat->visit_date,
+                'trimester' => $stat->trimester,
+                'bmi' => $stat->bmi,
+                'weight' => $stat->weight,
+                'height' => $stat->height,
+                'blood_sugar' => $stat->blood_sugar,
+                'blood_pressure' => $stat->blood_pressure,
+                'health_status' => $stat->health_status,
+                'fundal_height' => $stat->fundal_height,
+                'notes' => $stat->notes,
+            ];
+        }
+
+        return $resource;
+    }
+
+}
