1e522636c0a99e37f1b08e966a9b6cb59cdcbd34
mani-hash <manimehalan400@gmail.com>
2025-10-23 01:16:15 +0530

    feat: added favicon icon [TASK-92]

--- resources/views/layout/main.view.php
+++ resources/views/layout/main.view.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}">
     <link rel="stylesheet" href="css/app.css">
     @yield('css')
 </head>
--- resources/views/layout/portal.view.php
+++ resources/views/layout/portal.view.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/pages/portal.css') }}">
     
