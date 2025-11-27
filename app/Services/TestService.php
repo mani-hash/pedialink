4e79b9657213151eb69d2a433b52a61db1385619
Keeththi2003 <keeththi2003@gmail.com>
2025-11-27 17:39:35 +0530

    feat: implement search functionality in TestService to filter test results

--- app/Services/TestService.php
+++ app/Services/TestService.php
@@ -29,6 +29,41 @@ class TestService
         return $resource;
     }
 
+
+    public function getSearchResults($search){
+        $tests = Test::query()->get();
+
+        $resource = [];
+
+        foreach ($tests as $test) {
+            $resource[] = [
+                "id" => $test->id,
+                "name" => $test->name,
+                "category" => $test->category,
+                "stock" => $test->stock,
+                "price" => $test->price,
+                "created_at" => $test->created_at,
+               
+            ];
+        }
+
+         if ($search) {
+        $searchResutls = array_filter($resource, function ($row) use ($search) {
+            $s = strtolower($search);
+            return str_contains(strtolower($row['name']), $s)
+                || str_contains(strtolower($row['category']), $s)
+                || str_contains(strtolower($row['price']), $s);
+        });
+    }
+
+    return $searchResutls;
+
+
+
+    }
+
 }
+
+
 
 ?>
