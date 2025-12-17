c189d2eee48f93cb11add49c2225d0b2a0e4fd08
mani-hash <manimehalan400@gmail.com>
2025-12-17 23:20:32 +0530

    feat: pass additional data [TASK-123]

--- app/Services/Admin/UserOverviewService.php
+++ app/Services/Admin/UserOverviewService.php
@@ -13,13 +13,52 @@ class UserOverviewService
         $resource = [];
 
         foreach ($users as $user) {
-            $resource[] = [
+            $common = [
                 "id" => $user->id,
                 "name" => $user->name,
                 "email" => $user->email,
                 "role" => $user->role,
                 "email_verified_at" => false,
+                "created_at" => $user->created_at,
             ];
+
+            $additional = [];
+
+            switch ($user->role) {
+                case "parent":
+                    $parent = $user->getRole();
+                    $additional = [
+                        "nic" => $parent->nic,
+                        "type" => $parent->type,
+                        "area" => $parent->getArea()?->code,
+                    ];
+                    break;
+                case "phm":
+                    $phm = $user->getRole();
+                    $staff = $phm->getStaff();
+                    $additional = [
+                        "nic" => $staff->nic,
+                        "license_no" => $staff->license_no,
+                        "area" => $phm->getArea()->code,
+                    ];
+                    break;
+                case "doctor":
+                    $doctor = $user->getRole();
+                    $staff = $doctor->getStaff();
+                    $additional = [
+                        "nic" => $staff->nic,
+                        "license_no" => $staff->license_no,
+                    ];
+                    break;
+                case "admin":
+                    $admin = $user->getRole();
+                    $additional = [
+                        "type" => $admin->getAdminType(),
+                    ];
+                    break;
+            }
+
+            $resource[] = array_merge($common, $additional);
         }
 
         return $resource;
