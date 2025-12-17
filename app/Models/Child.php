3c11103f8a499441222919826664d956cc7e37c9
Keeththi2003 <keeththi2003@gmail.com>
2025-12-18 01:44:57 +0530

    feat: create Child model with area and PHM relationship methods [TASK-126]

---
+++ app/Models/Child.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Library\Framework\Core\Model;
+
+class Child extends Model
+{
+    protected static string $table = "children";
+    protected array $fillable = ["phm_id", "date_of_birth", "gender", "birth_certificate", "blood_type","area_id"];
+
+    public function getArea(): object|null
+    {
+        return Area::find($this->area_id);
+    }
+
+    public function getPHM(): object|null
+    {
+        return PublicHealthMidwife::find($this->phm_id);
+    }
+}
