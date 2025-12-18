68cf8f5a5f2f8fbc714ad71be09d4c79626b1872
mani-hash <manimehalan400@gmail.com>
2025-12-18 14:24:56 +0530

    feat: improve filter controls and query searching [TASK-127]

--- resources/views/components/table/controls.view.php
+++ resources/views/components/table/controls.view.php
@@ -13,95 +13,66 @@ $filters = isset($filters) ? $filters : null;
     {{ $slots['search'] }}
     @else
 
-    <form id="search-form" method="GET" action="{{ $action }}">
+    <form data-merge-get id="search-form" method="GET" action="{{ $action }}">
 
       <div class="tc-search">
-        <c-input name="search" type="text" placeholder="Search">
+        <c-input name="search" type="text" placeholder="Search" value="{{ getQueryParam('search') ?? '' }}">
+          <c-slot name="suffix">
+            <c-button type="submit" size="sm" form="search-form">
+              <img src="{{ asset('assets/icons/search.svg') }}" />
+            </c-button>
+          </c-slot>
         </c-input>
-
-        <c-button type="submit" size="sm" form="search-form">
-          <img src="{{ asset('assets/icons/search.svg') }}" />
-        </c-button>
       </div>
     </form>
     @endif
 
     <div class="tc-filters">
       @if (!empty($filters) && is_array($filters))
-      @foreach ($filters as $filterName => $filterItems)
-      <div class="tc-actions column-dropdown">
-        <c-dropdown.main :closeOnSelect="false">
-          <c-slot name="trigger">
-            <c-button variant="outline" class="dropdown-trigger">
-              <img src="{{ asset('assets/icons/filter.svg') }}" />
-              <span>{{ $filterName }}</span>
-            </c-button>
-          </c-slot>
+        @foreach ($filters as $filterName => $filterItems)
+          <div class="tc-actions column-dropdown">
+            <c-dropdown.main :closeOnSelect="false">
+              <c-slot name="trigger">
+                <c-button variant="outline" class="dropdown-trigger">
+                  <img src="{{ asset('assets/icons/filter.svg') }}" />
+                  <span>{{ $filterName }}</span>
+                </c-button>
+              </c-slot>
 
-          <c-slot name="menu">
-            <form id="filter-form-{{$filterName}}" method="GET" action="{{$action}}">
-              @foreach ($filterItems as $item)
-              <c-dropdown.item>
-                <label style="display: flex; align-items:center; gap:.5rem;">
-                  <input type="checkbox" name="filters[{{ $filterName }}][]" value="{{ $item }}" />
-                  <span>{{ $item }}</span>
-                </label>
-              </c-dropdown.item>
-              @endforeach
+              <c-slot name="menu">
+                <form data-merge-get id="filter-form-{{$filterName}}" method="GET" action="{{$action}}">
+                  @foreach ($filterItems as $key => $item)
+                    <c-dropdown.item asChild>
+                      <c-checkbox
+                        name="filters[{{ $filterName }}][]"
+                        value="{{ $item }}"
+                        label="{{ $item }}"
+                        :checked="isFilterChecked('filters[' . $filterName . '][]', $item)"
+                      />
+                    </c-dropdown.item>
+                  @endforeach
 
-              <c-button class="filter-btn" type="submit" variant="primary" size="sm" form="filter-form-{{$filterName}}">
-                Apply
-              </c-button>
-            </form>
-          </c-slot>
-        </c-dropdown.main>
-      </div>
-      @endforeach
+                  <c-button class="filter-btn" type="submit" variant="primary" size="sm" form="filter-form-{{$filterName}}">
+                    Apply
+                  </c-button>
+                </form>
+              </c-slot>
+            </c-dropdown.main>
+          </div>
+        @endforeach
+        <a href="{{$action}}">
+          <c-button variant="primary">
+            Reset
+          </c-button>
+        </a>
       @endif
-      <a href="{{$action}}">
-        <c-button variant="primary">
-          Reset
-        </c-button>
-      </a>
     </div>
-
-
-
-
   </div>
 
   <div class="table-controls__right">
     @if (!empty($slots['extrabtn']))
     <div class="tc-actions">
       {{ $slots['extrabtn'] }}
-    </div>
-    @endif
-
-    @if (!empty($columns) && is_array($columns))
-    <div class="tc-actions column-dropdown">
-      <c-dropdown.main :closeOnSelect="false">
-        <c-slot name="trigger">
-          <c-button variant="outline" class="dropdown-trigger">
-            <img src="{{ asset('assets/icons/arrow-down-01-round.svg') }}" alt="" />
-            <span>Columns</span>
-          </c-button>
-        </c-slot>
-
-        <c-slot name="menu">
-          <form id="col-form" method="GET" action="{{$action}}">
-            @foreach ($columns as $col)
-            <c-dropdown.item>
-              <label style="display:inline-flex; align-items:center; gap:.5rem; width:100%;">
-                <input type="checkbox" name="col[]" value="{{ $col }}" /> <span>{{ $col }}</span>
-              </label>
-            </c-dropdown.item>
-            @endforeach
-            <c-button class="filter-btn" type="submit" variant="primary" size="sm" form="col-form">
-              Apply
-            </c-button>
-          </form>
-        </c-slot>
-      </c-dropdown.main>
     </div>
     @endif
   </div>
