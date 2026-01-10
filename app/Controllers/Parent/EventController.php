545866a1070eabcb2befe3de62eb9df0b70d38c7
Keeththi2003 <keeththi2003@gmail.com>
2026-01-10 15:28:22 +0530

    feat: add booking functionality to EventController for event registrations [TASK-132]

--- app/Controllers/Parent/EventController.php
+++ app/Controllers/Parent/EventController.php
@@ -19,6 +19,38 @@ class EventController
 
         return view("parent/events-campaigns", ['events' => $events]);
     }
+
+    public function bookEvent($request)
+    {
+        $userId = auth()->user()->id;   
+
+        $eventId = $request->get('event_id');
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $phone = $request->get('phone');
+
+        // $errors = $this->adminUserService
+        //     ->validateAdminUser($name, $email, $type);
+
+        // if (count($errors) !== 0) {
+        //     return redirect(route("admin.user.admin"))
+        //         ->withInput([
+        //             "name" => $name,
+        //             "email" => $email
+        //         ])
+        //         ->withErrors($errors)
+        //         ->with("create", true);
+        // }
+
+        $this->eventService->bookEvent($eventId, $userId, $name, $email, $phone);
+
+        return redirect(route("parent.events.campaigns"))
+            ->withMessage(
+                "Event was successfully booked",
+                "Event Booked",
+                "success"
+            );
+    }
    
  
 }
