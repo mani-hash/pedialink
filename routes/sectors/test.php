d804f294021a53b6a8466d4d43eaccf57920936d
mani-hash <manimehalan400@gmail.com>
2026-01-10 20:36:53 +0530

    test: email test route [TASK-136]

--- routes/sectors/test.php
+++ routes/sectors/test.php
@@ -8,5 +8,5 @@ return [
     ['GET', '/test', [TestController::class, 'testPortal'], 'test.portal'],
     ['GET', '/test/calendar', [TestController::class, 'testCalendar'], 'test.calendar'],
     ['GET', '/test/message', [TestController::class, 'testMessage'], 'test.message'],
-
+    ['POST', '/test/mail/send', [TestController::class, 'sendMail'], 'test.mail'],
 ];
