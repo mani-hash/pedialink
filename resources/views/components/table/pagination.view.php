4a5f5a72d85d5f1a0b817921bfffd9068604ece0
mani-hash <manimehalan400@gmail.com>
2026-01-13 19:53:37 +0530

    feat: add pagination button support [TASK-139]

--- resources/views/components/table/pagination.view.php
+++ resources/views/components/table/pagination.view.php
@@ -1,10 +1,70 @@
 <div class="table-pagination-footer">
     <div class="table-pagination-btn-group">
-        <c-button variant="outline" >
-            Previous
-        </c-button>
-        <c-button variant="outline" >
-            Next
-        </c-button>
+        @if (isset($links))
+            @if ($links['current_page'] === 1)
+                <c-button variant="outline" disabled>
+                    Previous
+                </c-button>
+            @else
+                <c-link type="outline" id="previous">
+                    Previous
+                </c-link>
+            @endif
+
+            @if ($links['current_page'] === $links['last_page'])
+                <c-button variant="outline" disabled>
+                    Next
+                </c-button>
+            @else 
+                <c-link type="outline" id="next">
+                    Next
+                </c-link>
+            @endif
+        @else 
+            <c-button variant="outline" >
+                Previous
+            </c-button>
+            <c-button variant="outline" >
+                Next
+            </c-button>
+        @endif
     </div>
 </div>
+
+<script>
+    (() => {
+        function buildUrlWithParams(params, baseUrl = window.location.href) {
+            const url = new URL(baseUrl);
+    
+            Object.entries(params).forEach(([key, value]) => {
+                if (value === null || value === undefined) {
+                    url.searchParams.delete(key);
+                } else {
+                    url.searchParams.set(key, value);
+                }
+            });
+    
+            return url.toString();
+        }
+    
+        let next = {{ $links['current_page'] + 1 }};
+        let previous = {{ $links['current_page'] - 1 }}
+    
+        console.log("Heelooo");
+
+        const nextBtn = document.getElementById('next');
+    
+        if (nextBtn) {
+            nextBtn.href = 
+                buildUrlWithParams({ page: next });
+        }
+
+        const prevBtn = document.getElementById('previous');
+
+        if (prevBtn) {
+            prevBtn.href = 
+                buildUrlWithParams({ page: previous  });
+        }
+
+    })();
+</script>
