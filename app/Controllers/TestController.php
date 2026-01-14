3a47df9b98acb20f76d1fdebef47413c3b7ddc84
Keeththi2003 <keeththi2003@gmail.com>
2026-01-14 15:57:58 +0530

    refactor: streamline test portal logic by consolidating search and filter handling [TASK-141]

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -21,22 +21,10 @@ class TestController
         $search = $request->input("search");
         $filters = $request->input("filters");
 
-        if (!empty($search)) {
-            $searchResults = $this->testService->getSearchResults($search);
-
-            return view('test/test', ['items' => $searchResults]);
+        
 
 
-        }
-
-        if (!empty($filters)) {
-            $filteredResults = $this->testService->getFilteredResults($filters);
-
-            return view('test/test', ['items' => $filteredResults]);
-        }
-
-
-        $testDetails = $this->testService->getAllTestDetails();
+        $testDetails = $this->testService->getAllTestDetails($search, $filters);
         return view('test/test', ['items' => $testDetails]);
     }
 
